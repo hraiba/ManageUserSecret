89d002c79a8d8b51fa33736aedbd79c89672d6ba
Mohammad Hraiba <hraibam@gmail.com>
2025-01-16 21:23:38 +0100

    some changes

--- src/csProj.ts
+++ src/csProj.ts
@@ -120,6 +120,7 @@ export const ensureUserSecretFile = async (
       await vscode.workspace.fs.writeFile(uri, content);
     }
 
+    // some comment to check the git changes
     const fileContent = await vscode.workspace.fs.readFile(uri);
     return new TextDecoder().decode(fileContent);
   } catch (error: any) {
